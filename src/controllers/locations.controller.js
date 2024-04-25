3e3e1aa0b21dc8fcdaa82347e9621efe6800c17b
Alana Maia <alanaalvesmaia@gmail.com>
2024-04-24 21:40:02 -0300

    feat: locations CRUD

--- src/controllers/locations.controller.js
+++ src/controllers/locations.controller.js
@@ -1,1 +1,68 @@
-//locations controller
+import { prismaClient } from "../database/prisma.client.js";
+
+export const readAllLocations = async (request, response) => {
+  const locations = await prismaClient.location.findMany();
+
+  response.status(200).json(locations);
+};
+
+export const readLocationById = async (request, response) => {
+  const { id } = request.params;
+
+  const location = await prismaClient.location.findUnique({
+    where: { id: Number(id) },
+    include: { event: true },
+  });
+
+  return response.status(200).json(location);
+};
+
+export const createLocation = async (request, response) => {
+  const { name, address, zip_code, city, state, link_maps } = request.body;
+
+  const location = await prismaClient.location.create({
+    data: {
+      name,
+      address,
+      zip_code,
+      city,
+      state,
+      link_maps,
+    },
+  });
+
+  response.status(201).json(location);
+};
+
+export const updateLocation = async (request, response) => {
+  const { id } = request.params;
+  const { name, address, zip_code, city, state, link_maps } = request.body;
+
+  const location = await prismaClient.location.update({
+    where: {
+      id: Number(id),
+    },
+    data: {
+      name,
+      address,
+      zip_code,
+      city,
+      state,
+      link_maps,
+    },
+  });
+
+  return response.status(200).json(location);
+};
+
+export const deleteLocation = async (request, response) => {
+  const { id } = request.params;
+
+  await prismaClient.location.delete({
+    where: {
+      id: Number(id),
+    },
+  });
+
+  response.status(204).send();
+};
--- src/routers/locations.router.js
+++ src/routers/locations.router.js
@@ -1,3 +1,16 @@
 import { Router } from "express";
+import {
+  createLocation,
+  deleteLocation,
+  readAllLocations,
+  readLocationById,
+  updateLocation,
+} from "../controllers/locations.controller.js";
 
 export const locationsRouter = Router();
+
+locationsRouter.get("/locations", readAllLocations);
+locationsRouter.post("/location", createLocation);
+locationsRouter.get("/location/:id", readLocationById);
+locationsRouter.put("/location/:id", updateLocation);
+locationsRouter.delete("/location/:id", deleteLocation);
