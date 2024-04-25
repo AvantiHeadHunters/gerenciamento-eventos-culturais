60bbe072154da41db1558f31af654e817ce75036
José Diogo Lobato Ribeiro <jdiogolobato@gmail.com>
2024-04-25 17:19:53 -0300

    feat: include readEventById in controller

--- src/controllers/events.controller.js
+++ src/controllers/events.controller.js
@@ -5,3 +5,14 @@ export const readAllEvents = async (_request, response) => {
 
   return response.status(200).json(events);
 };
+
+export const readEventById = async (request, response) => {
+  const { id } = request.params;
+
+  const event = await prismaClient.event.findUnique({
+    where: { id: Number(id)},
+    include: { location: true },
+  });
+
+  return response.status(200).json(event);
+};
