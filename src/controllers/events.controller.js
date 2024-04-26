829eb71519fed844ff05307f8bf93cc1a31370c5
José Diogo Lobato Ribeiro <jdiogolobato@gmail.com>
2024-04-26 15:22:19 -0300

    feat: include update and delete in event controller

--- src/controllers/events.controller.js
+++ src/controllers/events.controller.js
@@ -7,12 +7,80 @@ export const readAllEvents = async (_request, response) => {
 };
 
 export const readEventById = async (request, response) => {
+  try {
+    const { id } = request.params;
+
+    const event = await prismaClient.event.findUnique({
+      where: { id: Number(id) },
+      include: {
+        location: {
+          select: {
+            name: true,
+            address: true,
+            city: true,
+            state: true,
+          },
+        },
+      }
+    });
+
+    if (!event) return response.status(404).json({ error: "Event not found" });
+    return response.status(200).json(event);
+  } catch (error) {
+    return response.status(500).json({ error: "Internal server error" });
+  }
+};
+
+export const createEvent = async (request, response) => {
+  try {
+    const { name, description, date, locationId } = request.body;
+
+    const event = await prismaClient.event.create({
+      data: {
+        name,
+        description,
+        date,
+        locationId,
+      },
+    });
+
+    return response.status(201).json(event);
+  } catch (error) {
+    return response.status(500).json({ error: "Internal server error" });
+  }
+};
+
+export const updateEvent = async (request, response) => {
+  try {
+    const { id } = request.params;
+    const { name, description, date, locationId } = request.body;
+
+    const event = await prismaClient.event.update({
+      where: {
+        id: Number(id),
+      },
+      data: {
+        name,
+        description,
+        date,
+        locationId,
+      },
+    });
+
+    return response.status(200).json(event);
+  } catch (error) {
+    return response.status(500).json({ error: "Internal server error" });
+  }
+};
+
+export const deleteEvent = async (request, response) => {
   const { id } = request.params;
 
-  const event = await prismaClient.event.findUnique({
-    where: { id: Number(id)},
-    include: { location: true },
+  await prismaClient.event.delete({
+    where: {
+      id: Number(id),
+    },
   });
 
-  return response.status(200).json(event);
+  return response.status(204).send();
 };
--- src/routers/events.router.js
+++ src/routers/events.router.js
@@ -1,6 +1,16 @@
 import { Router } from "express";
-import { readAllEvents } from "../controllers/events.controller.js";
+import {
+    readAllEvents,
+    readEventById,
+    createEvent,
+    updateEvent,
+    deleteEvent,
+} from "../controllers/events.controller.js";
 
 export const eventsRouter = Router();
 
 eventsRouter.get("/events", readAllEvents);
+eventsRouter.get("/event/:id", readEventById);
+eventsRouter.post("/event", createEvent);
+eventsRouter.put("/event/:id", updateEvent);
+eventsRouter.delete("/event/:id", deleteEvent);
