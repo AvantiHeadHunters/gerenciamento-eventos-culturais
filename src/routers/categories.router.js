655dd1b0a17ebf314d0d9db0a865b6e05e7fac1e
HelenAndrade <helenaraujoandrade@hotail.com>
2024-04-29 12:53:23 -0300

    feat: create categories router

--- src/routers/categories.router.js
+++ src/routers/categories.router.js
@@ -1,1 +1,16 @@
-// categories router
+import { Router } from "express";
+import {
+  readAllCategories,
+  readCategoryById,
+  createCategory,
+  updateCategory,
+  deleteCategory,
+} from "../controllers/categories.controller.js";
+
+export const categoriesRouter = Router();
+
+categoriesRouter.get("/categories", readAllCategories);
+categoriesRouter.get("/category/:id", readCategoryById);
+categoriesRouter.post("/category", createCategory);
+categoriesRouter.put("/category/:id", updateCategory);
+categoriesRouter.delete("/category/:id", deleteCategory);
