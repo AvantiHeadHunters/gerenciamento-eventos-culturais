00115391196d83560d1b2e8b50c26ac0d84e9d30
José Diogo Lobato Ribeiro <jdiogolobato@gmail.com>
2024-04-30 11:00:58 -0300

    feat: includes event middleware

--- src/routers/events.router.js
+++ src/routers/events.router.js
@@ -6,11 +6,14 @@ import {
     updateEvent,
     deleteEvent,
 } from "../controllers/events.controller.js";
+import { verifyEventExists } from "../middlewares/events.middleware.js";
 
 export const eventsRouter = Router();
 
 eventsRouter.get("/events", readAllEvents);
-eventsRouter.get("/event/:id", readEventById);
 eventsRouter.post("/event", createEvent);
+
+eventsRouter.use("/event/:id", verifyEventExists);
+eventsRouter.get("/event/:id", readEventById);
 eventsRouter.put("/event/:id", updateEvent);
 eventsRouter.delete("/event/:id", deleteEvent);
