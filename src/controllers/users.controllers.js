44d9e1f06bebebd014869e113fa6a124e0fb03c0
HelenAndrade <helenaraujoandrade@hotail.com>
2024-04-30 21:55:31 -0300

    feat: creat read users

--- src/controllers/users.controllers.js
+++ src/controllers/users.controllers.js
@@ -1,1 +1,28 @@
-// users.controllers.js
+import { prismaClient } from "../database/prisma.client.js";
+
+export const readAllUsers = async (_request, response) => {
+  try {
+    const users = await prismaClient.user.findMany();
+
+    return response.status(200).json(users);
+  } catch (error) {
+    console.log(error);
+    return response.status(500).json({ error: "Internal server error" });
+  }
+};
+
+export const readUserById = async (request, response) => {
+  try {
+    const { id } = request.params;
+
+    const user = await prismaClient.user.findUnique({
+      where: { id: Number(id) },
+      include: { event: true },
+    });
+
+    return response.status(200).json(user);
+  } catch (error) {
+    console.log(error);
+    return response.status(500).json({ error: "Internal server error" });
+  }
+};
