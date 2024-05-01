959b61223493ef654cf3bd07acc5a84bc24df2fa
José Diogo Lobato Ribeiro <jdiogolobato@gmail.com>
2024-04-30 22:17:51 -0300

    feat: user router

--- src/routers/users.router.js
+++ src/routers/users.router.js
@@ -1,1 +1,8 @@
-// users.router.js
+import { Router } from "express";
+import { readAllUsers, readUserById } from "../controllers/users.controllers";
+
+
+export const usersRouter = Router();
+
+usersRouter.get("/users", readAllUsers);
+usersRouter.get("/user/:id", readUserById);
